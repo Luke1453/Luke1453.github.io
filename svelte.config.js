fecebeefea244b4246cbb95467a471e26c2f141c
Luke1453 <lukjut@gmail.com>
2023-08-17 06:44:07 +0300

    feat: init skeleton

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,28 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev')
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess({})],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: "docs",
-			assets: "docs",
+			pages: 'docs',
+			assets: 'docs',
 			fallback: undefined,
 			precompress: false,
 			strict: true
 		}),
 		paths: {
-			base: dev ? "" : ""
-		},
+			base: dev ? '' : ''
+		}
 		// hydrate the <div id="svelte"> element in src/app.html
 		// target: "#svelte"
 	}
